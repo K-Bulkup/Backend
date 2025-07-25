89ba4b5da9ef3da048741fe1ec5b8a592f64aef1
chaeniiiii <kce2360@gmail.com>
2025-07-25 23:43:00 +0900

    Feat : 트레이너 프로필 조회 쿼리 설계

---
+++ src/main/java/com/kbulkup/profile/mapper/TrainerProfileMapper.java
@@ -0,0 +1,14 @@
+package com.kbulkup.profile.mapper;
+
+import com.kbulkup.profile.dto.response.TrainerProfileDetailResponseDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Optional;
+
+@Mapper
+public interface TrainerProfileMapper {
+
+    Optional<TrainerProfileDetailResponseDTO> getTrainerProfile(@Param("trainerId") Long TrainerId);
+
+}
