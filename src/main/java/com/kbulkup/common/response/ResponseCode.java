effb96cd9c85127e6b524e1b01effce85f7989a0
chaeniiiii <kce2360@gmail.com>
2025-07-25 23:37:22 +0900

    Feat : 트레이너 프로필 조회 예외 응답코드 생성

--- src/main/java/com/kbulkup/common/response/ResponseCode.java
+++ src/main/java/com/kbulkup/common/response/ResponseCode.java
@@ -11,7 +11,7 @@ public enum ResponseCode {
 
     //400 BadRequest
     VALIDATION_ERROR(BAD_REQUEST, "사용자 입력값이 올바르지 않습니다."),
-
+    NOT_FOUND_TRAINER_PROFILE(BAD_REQUEST,"트레이너 프로필 정보를 찾을 수 없습니다."),
     //200 OK
     SUCCESS(OK,"성공했습니다.");
 
