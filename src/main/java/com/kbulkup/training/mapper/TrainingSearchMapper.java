dfde6de547e2eba54ae609d696dee4d32d6da47f
saym010116 <saym0116@hanyang.ac.kr>
2025-07-25 13:10:12 +0900

    Feat: 트레이닝 검색용 MyBatis Mapper 및 SQL 쿼리 추가

--- src/main/java/com/kbulkup/training/mapper/TrainingSearchMapper.java
+++ src/main/java/com/kbulkup/training/mapper/TrainingSearchMapper.java
@@ -1,4 +1,11 @@
 package com.kbulkup.training.mapper;
 
-public class TrainingSearchMapper {
+import com.kbulkup.training.dto.response.TrainingSearchListResponseDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface TrainingSearchMapper {
+    List<TrainingSearchListResponseDTO> searchTrainings(String keyword);
 }
