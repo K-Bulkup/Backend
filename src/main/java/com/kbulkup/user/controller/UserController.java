a93cf171505b4d82e08d215e7e41ad11b227e6aa
Jung-kr <jungwy98@gmail.com>
2025-07-27 19:58:03 +0900

    Feat: 피니아 저장용 사용자 정보 조회 API 추가 (/api/common/users/me)
    - JWT에서 userId, roles 추출하여 반환하는 getMyInfo API 구현
    - JwtTokenProvider 통해 토큰 파싱
    - UserService 및 UserResponseDTO 정의

--- src/main/java/com/kbulkup/user/controller/UserController.java
+++ src/main/java/com/kbulkup/user/controller/UserController.java
@@ -1,4 +1,28 @@
 package com.kbulkup.user.controller;
 
+import com.kbulkup.common.response.CustomResponse;
+import com.kbulkup.common.response.ResponseCode;
+import com.kbulkup.common.security.JwtTokenProvider;
+import com.kbulkup.user.dto.response.UserResponseDTO;
+import com.kbulkup.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/common/users")
 public class UserController {
+
+    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @GetMapping("/me")
+    public CustomResponse<UserResponseDTO> getMyInfo(HttpServletRequest request) {
+        String token = jwtTokenProvider.resolveToken(request);
+        return CustomResponse.success(ResponseCode.SUCCESS, userService.getUserFromToken(token));
+    }
 }
