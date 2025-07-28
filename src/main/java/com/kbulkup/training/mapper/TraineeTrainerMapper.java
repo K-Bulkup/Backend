f02f8c050c6ad14667496700ef6007a85ee616c2
saym010116 <saym0116@hanyang.ac.kr>
2025-07-28 16:47:32 +0900

    feat(trainer-detail): 트레이너 상세 조회 Mapper 인터페이스 추가

--- src/main/java/com/kbulkup/training/mapper/TraineeTrainerMapper.java
+++ src/main/java/com/kbulkup/training/mapper/TraineeTrainerMapper.java
@@ -1,4 +1,13 @@
 package com.kbulkup.training.mapper;
 
-public class TraineeTrainerMapper {
+import com.kbulkup.training.dto.response.TrainingTrainerDetailProfileResonseDTO;
+import com.kbulkup.training.dto.response.TrainerTrainingSummaryDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface TraineeTrainerMapper {
+    TrainingTrainerDetailProfileResonseDTO selectTrainerProfile(Long trainerId);
+    List<TrainerTrainingSummaryDTO> selectTrainerTrainings(Long trainerId);
 }
