caf001a949a185f13a7b1eaf93b680fd85edd930
saym010116 <saym0116@hanyang.ac.kr>
2025-07-28 16:48:45 +0900

    Feat: 트레이너 상세 응답 DTO 추가

--- src/main/java/com/kbulkup/training/dto/response/TrainerDetailResponseDTO.java
+++ src/main/java/com/kbulkup/training/dto/response/TrainerDetailResponseDTO.java
@@ -1,4 +1,18 @@
 package com.kbulkup.training.dto.response;
 
+import java.util.List;
+
 public class TrainerDetailResponseDTO {
+
+    private TrainingTrainerDetailProfileResonseDTO trainer;
+    private List<TrainerTrainingSummaryDTO> trainings;
+
+    public TrainerDetailResponseDTO(TrainingTrainerDetailProfileResonseDTO trainer,
+                                    List<TrainerTrainingSummaryDTO> trainings) {
+        this.trainer = trainer;
+        this.trainings = trainings;
+    }
+
+    public TrainingTrainerDetailProfileResonseDTO getTrainer() { return trainer; }
+    public List<TrainerTrainingSummaryDTO> getTrainings() { return trainings; }
 }
