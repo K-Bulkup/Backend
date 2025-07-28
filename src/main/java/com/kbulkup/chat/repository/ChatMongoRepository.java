e54535ac396a0af7d75f23a5ff1b4e719d99d4f1
Jung-kr <jungwy98@gmail.com>
2025-07-28 13:32:21 +0900

    Feat: MongoDB 채팅 메시지 저장 및 조회 기능 구현
    - chat_messages 컬렉션에 채팅 메시지를 저장하는 save 메서드 구현
    - roomId 기준으로 채팅 내역을 조회하고, sendAt 기준 오름차순 정렬 적용

---
+++ src/main/java/com/kbulkup/chat/repository/ChatMongoRepository.java
@@ -0,0 +1,29 @@
+package com.kbulkup.chat.repository;
+
+import com.kbulkup.chat.domain.MongoChatMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ChatMongoRepository {
+
+    private final MongoTemplate mongoTemplate;
+
+    public void save(MongoChatMessage mongoChatMessage) {
+        mongoTemplate.save(mongoChatMessage);
+    }
+
+    public List<MongoChatMessage> findByRoomId(String roomId) {
+        Query query = new Query(Criteria.where("roomId").is(roomId));
+        query.with(Sort.by(Sort.Direction.ASC, "sendAt"));
+
+        return mongoTemplate.find(query, MongoChatMessage.class, "chat_messages");
+    }
+}
