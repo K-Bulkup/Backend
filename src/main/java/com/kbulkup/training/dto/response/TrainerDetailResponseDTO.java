b0df1e7d5bde12de00d2d9b74e4a299b06b4ab02
saym010116 <saym0116@hanyang.ac.kr>
2025-07-29 10:14:04 +0900

    Refactor: 팩토리 메서드를 사용하여 객체 생성하는 방식으로 변경

--- src/main/java/com/kbulkup/training/dto/response/TrainerDetailResponseDTO.java
+++ src/main/java/com/kbulkup/training/dto/response/TrainerDetailResponseDTO.java
@@ -1,6 +1,7 @@
 package com.kbulkup.training.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import java.util.List;
@@ -8,8 +9,18 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class TrainerDetailResponseDTO {
 
     private TrainingTrainerDetailProfileResponseDTO trainer;
     private List<TrainerTrainingSummaryDTO> trainings;
+
+    public static TrainerDetailResponseDTO create(TrainingTrainerDetailProfileResponseDTO trainer,
+                                                  List<TrainerTrainingSummaryDTO> trainings) {
+        return TrainerDetailResponseDTO.builder()
+                .trainer(trainer)
+                .trainings(trainings)
+                .build();
+    }
 }
+
