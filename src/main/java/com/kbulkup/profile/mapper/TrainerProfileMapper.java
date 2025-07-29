9312ff037fad79f6de5dda51b27083a94deec13b
chaeniiiii <kce2360@gmail.com>
2025-07-29 11:04:27 +0900

    Fix: 트레이너 프로필 조회 메서드명 변경

--- src/main/java/com/kbulkup/profile/mapper/TrainerProfileMapper.java
+++ src/main/java/com/kbulkup/profile/mapper/TrainerProfileMapper.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 @Mapper
 public interface TrainerProfileMapper {
 
-    Optional<TrainerProfileDetailResponseDTO> getTrainerProfile(@Param("trainerId") Long TrainerId);
+    Optional<TrainerProfileDetailResponseDTO> findByTrainerId(@Param("trainerId") Long trainerId);
     boolean updateTrainerCareer(@Param("trainerId") Long trainerId, @Param("career") String career);
     boolean updateTrainerProfileImage(@Param("trainerId") Long trainerId, @Param("profileImageUrl") String profileImageUrl);
 
