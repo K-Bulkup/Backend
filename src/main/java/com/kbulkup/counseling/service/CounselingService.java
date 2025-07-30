1f2f8bc89684f87d1b319fa13d5334fc158958eb
Jung-kr <jungwy98@gmail.com>
2025-07-30 19:21:41 +0900

    Feat: 상담 상세 조회 시 상대방 정보 동적 식별 및 응답 구조 개선
    - roomId와 내 userId를 기반으로 상대방(trainer 또는 trainee) 정보 조회 기능 추가
    - 하나의 엔드포인트로 trainer, trainee 공통 상담 상세 정보 조회 가능하도록 개선

--- src/main/java/com/kbulkup/counseling/service/CounselingService.java
+++ src/main/java/com/kbulkup/counseling/service/CounselingService.java
@@ -2,13 +2,13 @@ package com.kbulkup.counseling.service;
 
 import com.kbulkup.counseling.dto.response.CounselingCreateResponseDTO;
 import com.kbulkup.counseling.dto.response.CounselingDetailResponseDTO;
-import com.kbulkup.counseling.dto.response.TrainerCounselingListResponseDTO;
+import com.kbulkup.counseling.dto.response.CounselingListResponseDTO;
 
 import java.util.List;
 
 public interface CounselingService {
 
-    List<TrainerCounselingListResponseDTO> getCounselings(Long userId);
+    List<CounselingListResponseDTO> getCounselings(Long userId);
 
     CounselingCreateResponseDTO createOrGetCounselingsRoom(Long traineeId, Long trainingId);
 
