05df4fa9a9392033d1a68ee2727f5bf7e2264223
saym010116 <saym0116@hanyang.ac.kr>
2025-07-30 13:44:33 +0900

    Fix: 주석 수정

--- src/main/java/com/kbulkup/training/mapper/TraineeTrainingMapper.java
+++ src/main/java/com/kbulkup/training/mapper/TraineeTrainingMapper.java
@@ -24,6 +24,7 @@ public interface TraineeTrainingMapper {
     void updateTrainingProgress(@Param("trainingId") Long trainingId,
                                 @Param("userId") Long userId,
                                 @Param("progress") int progress);
-    /** ✅ 승인된 트레이닝 전체 목록 조회 */
+
+    /** 승인된 트레이닝 전체 목록 조회 */
     List<TraineeTrainingListResponseDTO> findAllApprovedTrainings();
 }
