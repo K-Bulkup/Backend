4fae99686acdd255133893650e34554814547f1f
saym010116 <saym0116@hanyang.ac.kr>
2025-07-30 11:43:57 +0900

    Feat : 트레이닝 상세 조회용 findTrainingDetail 메서드 TraineeTrainingMapper에 추가

--- src/main/java/com/kbulkup/training/mapper/TraineeTrainingMapper.java
+++ src/main/java/com/kbulkup/training/mapper/TraineeTrainingMapper.java
@@ -2,6 +2,7 @@ package com.kbulkup.training.mapper;
 
 import com.kbulkup.routine.dto.RoutineSummaryResponseDTO;
 import com.kbulkup.routine.dto.TraineeRoutineSummaryResponseDTO;
+import com.kbulkup.training.dto.response.TraineeTrainingDetailResponseDTO;
 import com.kbulkup.training.dto.response.TraineeTrainingListResponseDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -24,6 +25,8 @@ public interface TraineeTrainingMapper {
     void updateTrainingProgress(@Param("trainingId") Long trainingId,
                                 @Param("userId") Long userId,
                                 @Param("progress") int progress);
-    /** ✅ 승인된 트레이닝 전체 목록 조회 */
+    /** 승인된 트레이닝 전체 목록 조회 */
     List<TraineeTrainingListResponseDTO> findAllApprovedTrainings();
+
+    TraineeTrainingDetailResponseDTO findTrainingDetail(@Param("trainingId") Long trainingId);
 }
