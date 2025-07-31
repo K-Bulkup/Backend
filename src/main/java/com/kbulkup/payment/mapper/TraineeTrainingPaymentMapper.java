4058ceffdfd7544df9d13cabd332b8d304aa3bb6
saym010116 <saym0116@hanyang.ac.kr>
2025-07-31 14:52:43 +0900

    Feat : enrollments 중복 방지 로직 및 Mapper 추가

--- src/main/java/com/kbulkup/payment/mapper/TraineeTrainingPaymentMapper.java
+++ src/main/java/com/kbulkup/payment/mapper/TraineeTrainingPaymentMapper.java
@@ -1,11 +1,15 @@
 package com.kbulkup.payment.mapper;
 
-import com.kbulkup.payment.domain.TraineeTrainingPayment;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface TraineeTrainingPaymentMapper {
-    void insertTraineeTrainingPayment(TraineeTrainingPayment payment);
-    void enrollUserToTraining(@Param("userId") Long userId, @Param("trainingId") Long trainingId);
+
+    /**
+     * 결제 성공 시 수강 등록(enrollments) 테이블에 신규 row 추가
+     * @param userId 결제한 사용자 ID
+     * @param trainingId 결제한 강의 ID
+     */
+    void insertEnrollment(@Param("userId") Long userId, @Param("trainingId") Long trainingId);
 }
