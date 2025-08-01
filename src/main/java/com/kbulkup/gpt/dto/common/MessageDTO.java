67cebfedd01fc847db7b128ff50bac34f3bb2e81
HyoSangByun <billy04100410@gmail.com>
2025-08-01 09:50:27 +0900

    Feat: GPT DTO 추가

---
+++ src/main/java/com/kbulkup/gpt/dto/common/MessageDTO.java
@@ -0,0 +1,36 @@
+package com.kbulkup.gpt.dto.common;
+
+import com.kbulkup.gpt.dto.request.ImageContentDTO;
+import com.kbulkup.gpt.dto.request.TextContentDTO;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageDTO {
+    private String role; //user, assistant, system
+    private Object content;
+
+    // ✅ 텍스트만 담긴 메시지 생성
+    public static MessageDTO createOnlyText(String role, String text) {
+        return MessageDTO.builder()
+                .role(role)
+                .content(text)
+                .build();
+    }
+
+    // ✅ 텍스트 + 이미지 메시지 생성
+    public static MessageDTO createTextAndImage(String role, String text, String imageUrl) {
+        return MessageDTO.builder()
+                .role(role)
+                .content(List.of(
+                        new TextContentDTO(text),
+                        new ImageContentDTO(imageUrl)
+                ))
+                .build();
+    }
+}
