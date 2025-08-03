e044ede37295e64a368c564062ad0faf70f70b61
chaeniiiii <kce2360@gmail.com>
2025-08-03 16:44:49 +0900

    Feat : 트레이너 프로필 이미지 S3 업로드

--- src/main/java/com/kbulkup/profile/mapper/TrainerProfileMapper.java
+++ src/main/java/com/kbulkup/profile/mapper/TrainerProfileMapper.java
@@ -11,7 +11,7 @@ public interface TrainerProfileMapper {
 
     Optional<TrainerProfileDetailResponseDTO> findByTrainerId(@Param("trainerId") Long trainerId);
     boolean updateTrainerCareer(@Param("trainerId") Long trainerId, @Param("career") String career);
-    boolean updateTrainerProfileImage(@Param("trainerId") Long trainerId, @Param("profileImageUrl") String profileImageUrl);
+    void updateTrainerProfileImage(@Param("trainerId") Long trainerId, @Param("profileImageUrl") String profileImageUrl);
     void insertInitialProfile(@Param("trainerId") Long trainerId);
 
 }
