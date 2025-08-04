81c7702cf64c82483b194714f7c8dd1877dbd673
Jung-kr <jungwy98@gmail.com>
2025-08-04 17:05:57 +0900

    Feat: AI 컨설팅 자산 정보 전송을 위한 isAsset 필드 추가 및 관련 로직 수정

--- src/main/java/com/kbulkup/gpt/service/GPTService.java
+++ src/main/java/com/kbulkup/gpt/service/GPTService.java
@@ -5,5 +5,5 @@ import com.kbulkup.gpt.dto.response.GPTResponseDTO;
 public interface GPTService {
     GPTResponseDTO requestOnlyText(String mission, String userAnswer);
     GPTResponseDTO requestImageAnalysis(String mission, String imageUrl);
-    GPTResponseDTO requestCounseling(String userId, String question);
+    GPTResponseDTO requestCounseling(String userId, String question, boolean isAsset);
 }
