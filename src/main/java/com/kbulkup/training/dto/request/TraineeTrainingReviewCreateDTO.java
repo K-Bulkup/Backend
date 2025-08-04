4ae23adaccae62ec391bad79c277c90107724a48
HyoSangByun <billy04100410@gmail.com>
2025-08-04 15:44:21 +0900

    Feat : 리뷰 DTO 생성

---
+++ src/main/java/com/kbulkup/training/dto/request/TraineeTrainingReviewCreateDTO.java
@@ -0,0 +1,15 @@
+package com.kbulkup.training.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TraineeTrainingReviewCreateDTO {
+    private int rating;
+    private String content;
+}
