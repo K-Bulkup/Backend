8549c60eae03596498eb18909224a1425faf2c12
chaeniiiii <kce2360@gmail.com>
2025-08-05 10:44:04 +0900

    Refactor : 트레이너 프로필 이미지 업로드 응답값 수정

--- src/main/java/com/kbulkup/profile/service/TrainerProfileService.java
+++ src/main/java/com/kbulkup/profile/service/TrainerProfileService.java
@@ -4,12 +4,13 @@ import com.kbulkup.common.response.CustomResponse;
 import com.kbulkup.profile.dto.request.TrainerProfileCareerUpdateRequestDTO;
 import com.kbulkup.profile.dto.request.TrainerProfileImageUpdateRequestDTO;
 import com.kbulkup.profile.dto.response.TrainerProfileDetailResponseDTO;
+import com.kbulkup.profile.dto.response.TrainerProfileImgUrlResponseDTO;
 
 public interface TrainerProfileService {
 
     TrainerProfileDetailResponseDTO getTrainerProfile(Long trainerId);
     CustomResponse<Void> updateTrainerProfileCareer(Long trainerId, TrainerProfileCareerUpdateRequestDTO dto);
-    void updateTrainerProfileImage(Long trainerId, TrainerProfileImageUpdateRequestDTO dto);
+    TrainerProfileImgUrlResponseDTO updateTrainerProfileImage(Long trainerId, TrainerProfileImageUpdateRequestDTO dto);
     void createInitialProfile(Long trainerId);
 
 }
