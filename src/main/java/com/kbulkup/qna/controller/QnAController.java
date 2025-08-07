2fc838d529e5db855faf71a9e9b5051d21e85b3b
HyoSangByun <billy04100410@gmail.com>
2025-08-07 11:37:23 +0900

    Feat : 질문 답변을 위한 컨트롤러

--- src/main/java/com/kbulkup/qna/controller/QnAController.java
+++ src/main/java/com/kbulkup/qna/controller/QnAController.java
@@ -3,6 +3,7 @@ package com.kbulkup.qna.controller;
 import com.kbulkup.common.response.CustomResponse;
 import com.kbulkup.common.response.ResponseCode;
 import com.kbulkup.qna.dto.request.TraineeTrainingQnARequestDTO;
+import com.kbulkup.qna.dto.request.TrainerTrainingQnARequestDTO;
 import com.kbulkup.qna.dto.response.CommonTrainingQnAListResponseDTO;
 import com.kbulkup.qna.service.QnAService;
 import com.kbulkup.user.domain.User;
@@ -30,8 +31,9 @@ public class QnAController {
         return CustomResponse.success(ResponseCode.SUCCESS);
     }
 
-    /*@PostMapping("/trainer/qna")
-    public CustomResponse<Void> createTrainingAnswer(@AuthenticationPrincipal(expression = "user") User user) {
-
-    }*/
+    @PostMapping("/trainer/trainings/{trainingId}/answer")
+    public CustomResponse<Void> createTrainingAnswer(@RequestBody TrainerTrainingQnARequestDTO dto) {
+        qnAService.createTrainerTrainingAnswer(dto.getQnaId(), dto.getAnswer());
+        return CustomResponse.success(ResponseCode.SUCCESS);
+    }
 }
