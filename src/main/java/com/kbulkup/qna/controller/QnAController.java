6435d3ea8c119aab670c0625eaf8b64cd8e81db1
HyoSangByun <billy04100410@gmail.com>
2025-08-07 10:57:13 +0900

    Feature : QnA목록을 위한 컨트롤러

--- src/main/java/com/kbulkup/qna/controller/QnAController.java
+++ src/main/java/com/kbulkup/qna/controller/QnAController.java
@@ -3,11 +3,14 @@ package com.kbulkup.qna.controller;
 import com.kbulkup.common.response.CustomResponse;
 import com.kbulkup.common.response.ResponseCode;
 import com.kbulkup.qna.dto.request.TraineeTrainingQnARequestDTO;
+import com.kbulkup.qna.dto.response.CommonTrainingQnAListResponseDTO;
 import com.kbulkup.qna.service.QnAService;
 import com.kbulkup.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -16,10 +19,19 @@ public class QnAController {
 
     private final QnAService qnAService;
 
+    @GetMapping("/common/trainings/{trainingId}/qnas")
+    public CustomResponse<List<CommonTrainingQnAListResponseDTO>> getTrainingQnAs(@PathVariable Long trainingId) {
+        return CustomResponse.success(ResponseCode.SUCCESS, qnAService.getTrainingQnAs(trainingId));
+    }
+
     @PostMapping("/trainee/trainings/{trainingId}/question")
     public CustomResponse<Void> createTrainingQuestion(@AuthenticationPrincipal(expression = "user") User user, @PathVariable Long trainingId, @RequestBody TraineeTrainingQnARequestDTO dto) {
         qnAService.createTraineeTrainingQuestion(user.getUserId(), trainingId, dto.getQuestion());
         return CustomResponse.success(ResponseCode.SUCCESS);
     }
 
+    /*@PostMapping("/trainer/qna")
+    public CustomResponse<Void> createTrainingAnswer(@AuthenticationPrincipal(expression = "user") User user) {
+
+    }*/
 }
