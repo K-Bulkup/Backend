011291bd75fd025d15a63399bc4834b265c1758a
HyoSangByun <billy04100410@gmail.com>
2025-08-07 10:57:31 +0900

    Feature : QnA목록을 위한 서비스

--- src/main/java/com/kbulkup/qna/service/QnAService.java
+++ src/main/java/com/kbulkup/qna/service/QnAService.java
@@ -1,6 +1,12 @@
 package com.kbulkup.qna.service;
 
+import com.kbulkup.qna.dto.response.CommonTrainingQnAListResponseDTO;
+
+import java.util.List;
+
 public interface QnAService {
 
     void createTraineeTrainingQuestion(Long userId, Long traingId, String question);
+
+    List<CommonTrainingQnAListResponseDTO> getTrainingQnAs(Long trainingId);
 }
--- src/main/java/com/kbulkup/qna/service/QnAServiceImpl.java
+++ src/main/java/com/kbulkup/qna/service/QnAServiceImpl.java
@@ -1,20 +1,28 @@
 package com.kbulkup.qna.service;
 
+import com.kbulkup.qna.dto.response.CommonTrainingQnAListResponseDTO;
 import com.kbulkup.qna.mapper.QnAMapper;
 import com.kbulkup.training.mapper.TrainingMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class QnAServiceImpl implements QnAService {
 
-    private final QnAMapper qnAMapper;
+    private final QnAMapper qnaMapper;
     private final TrainingMapper trainingMapper;
+
+    @Override
+    public List<CommonTrainingQnAListResponseDTO> getTrainingQnAs(Long trainingId) {
+        return qnaMapper.getQnAList(trainingId);
+    }
 
     @Override
     public void createTraineeTrainingQuestion(Long traineeId, Long traingId, String question) {
         Long trainerId = trainingMapper.findTrainerByTrainingId(traingId);
-        qnAMapper.insertQuestion(traingId, traineeId, trainerId, question);
+        qnaMapper.insertQuestion(traingId, traineeId, trainerId, question);
     }
 }
