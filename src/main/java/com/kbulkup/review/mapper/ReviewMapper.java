233900b263baa9d133732b6164af9c6434aa1d0a
HyoSangByun <billy04100410@gmail.com>
2025-08-07 16:33:44 +0900

    Feat : 리뷰 작성 여부를 위한 쿼리

--- src/main/java/com/kbulkup/review/mapper/ReviewMapper.java
+++ src/main/java/com/kbulkup/review/mapper/ReviewMapper.java
@@ -2,6 +2,7 @@ package com.kbulkup.review.mapper;
 
 import com.kbulkup.review.dto.response.TrainerTrainingReviewDetailResponseDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -9,4 +10,6 @@ import java.util.List;
 public interface ReviewMapper {
 
     List<TrainerTrainingReviewDetailResponseDTO> findReviewsByTrainingId(Long trainingId);
+
+    Boolean hasWrittenReview(@Param("userId") Long userId,@Param("trainingId") Long trainingId);
 }
