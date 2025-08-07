0b678d90e149e24dec7741ca9bf6390ffdbe5973
HyoSangByun <billy04100410@gmail.com>
2025-08-07 16:33:34 +0900

    Feat : 리뷰 작성 여부를 위한 서비스

--- src/main/java/com/kbulkup/review/service/ReviewService.java
+++ src/main/java/com/kbulkup/review/service/ReviewService.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface ReviewService {
     List<TrainerTrainingReviewDetailResponseDTO> getTrainingReviews(Long trainingId);
+
+    Boolean hasWrittenReview(Long userId, Long trainingId);
 }
--- src/main/java/com/kbulkup/review/service/ReviewServiceImpl.java
+++ src/main/java/com/kbulkup/review/service/ReviewServiceImpl.java
@@ -17,4 +17,9 @@ public class ReviewServiceImpl implements ReviewService {
     public List<TrainerTrainingReviewDetailResponseDTO> getTrainingReviews(Long trainingId) {
         return reviewMapper.findReviewsByTrainingId(trainingId);
     }
+
+    @Override
+    public Boolean hasWrittenReview(Long userId, Long trainingId) {
+        return reviewMapper.hasWrittenReview(userId, trainingId);
+    }
 }
