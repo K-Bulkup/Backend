127efb11d8af7e5b39562f4ca33016ab350eb1ad
HyoSangByun <billy04100410@gmail.com>
2025-08-07 11:16:05 +0900

    Refactor : QnA목록 구조 변경

--- src/main/java/com/kbulkup/qna/dto/response/CommonTrainingQnAListResponseDTO.java
+++ src/main/java/com/kbulkup/qna/dto/response/CommonTrainingQnAListResponseDTO.java
@@ -12,14 +12,16 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommonTrainingQnAListResponseDTO {
+    private Long qnaId;
     private Long userId;
     private String question;
     private String answer;
     private LocalDateTime createAt;
     private LocalDateTime answeredAt;
 
-    public static CommonTrainingQnAListResponseDTO create(Long userId, String question, String answer, LocalDateTime createAt, LocalDateTime answeredAt) {
+    public static CommonTrainingQnAListResponseDTO create(Long qnaId, Long userId, String question, String answer, LocalDateTime createAt, LocalDateTime answeredAt) {
         return CommonTrainingQnAListResponseDTO.builder()
+                .qnaId(qnaId)
                 .userId(userId)
                 .question(question)
                 .answer(answer)
