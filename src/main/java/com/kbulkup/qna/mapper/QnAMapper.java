17d87817d28582c7894a4ccc469ed54bfcad1364
HyoSangByun <billy04100410@gmail.com>
2025-08-07 10:57:21 +0900

    Feature : QnA목록을 위한 쿼리

--- src/main/java/com/kbulkup/qna/mapper/QnAMapper.java
+++ src/main/java/com/kbulkup/qna/mapper/QnAMapper.java
@@ -1,7 +1,10 @@
 package com.kbulkup.qna.mapper;
 
+import com.kbulkup.qna.dto.response.CommonTrainingQnAListResponseDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface QnAMapper {
@@ -12,4 +15,6 @@ public interface QnAMapper {
             @Param("trainerId") Long trainerId,
             @Param("question") String question
     );
+
+    List<CommonTrainingQnAListResponseDTO> getQnAList(@Param("trainingId") Long trainingId);
 }
