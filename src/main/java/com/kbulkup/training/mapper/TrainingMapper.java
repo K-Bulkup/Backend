2ddf49b90e53e4201ecdf6fe3fc9480f01962f3c
HyoSangByun <billy04100410@gmail.com>
2025-08-08 11:48:58 +0900

    Feat : 쿼리 추가

--- src/main/java/com/kbulkup/training/mapper/TrainingMapper.java
+++ src/main/java/com/kbulkup/training/mapper/TrainingMapper.java
@@ -1,12 +1,17 @@
 package com.kbulkup.training.mapper;
 
+import com.kbulkup.qna.dto.response.TrainerTrainingListDetailResponseDTO;
 import com.kbulkup.training.domain.Training;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface TrainingMapper {
     void createTraining(Training training);
 
     Long findTrainerByTrainingId(@Param("trainingId") Long trainingId);
+
+    List<TrainerTrainingListDetailResponseDTO> findTrainerTrainings(@Param("trainerId") Long trainerId);
 }
