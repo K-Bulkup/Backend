a71619d421f8ca9fa947bdf8ea7c89aca2381fee
saym010116 <saym0116@hanyang.ac.kr>
2025-08-18 11:38:36 +0900

    Docs : profile 컨트롤러/DTO Swagger 주석 추가

--- src/main/java/com/kbulkup/profile/controller/TraineeProfileController.java
+++ src/main/java/com/kbulkup/profile/controller/TraineeProfileController.java
@@ -5,12 +5,14 @@ import com.kbulkup.common.response.ResponseCode;
 import com.kbulkup.profile.dto.response.TraineeProfileDetailResponseDTO;
 import com.kbulkup.profile.service.TraineeProfileService;
 import com.kbulkup.user.domain.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
+@Api(tags = "Trainee Profile", description = "수강생 프로필 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/trainee/profiles")
@@ -18,8 +20,10 @@ public class TraineeProfileController {
 
     private final TraineeProfileService traineeProfileService;
 
+    @ApiOperation(value = "내 프로필 조회", notes = "로그인한 수강생의 프로필을 조회합니다.")
     @GetMapping("/me")
-    public CustomResponse<TraineeProfileDetailResponseDTO> getTrainerProfile(@AuthenticationPrincipal(expression = "user") User user) {
+    public CustomResponse<TraineeProfileDetailResponseDTO> getTrainerProfile(
+            @ApiIgnore @AuthenticationPrincipal(expression = "user") User user) {
         return CustomResponse.success(ResponseCode.SUCCESS, traineeProfileService.getTraineeProfile(user.getUserId()));
     }
 }
